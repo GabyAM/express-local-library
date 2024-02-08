87a7ae328c56a15212aca825a78cac3ef6cd7ba3
GabyAM <gdgabyam@gmail.com>
2024-02-08 10:44:08 -0300

    adds validations to schemas

--- models/authorModel.js
+++ models/authorModel.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const AuthorSchema = new Schema({
-	first_name: String,
-	family_name: String,
+	first_name: { type: String, required: true, maxLength: 100 },
+	family_name: { type: String, required: true, maxLength: 100 },
 	date_of_birth: Date,
 	date_of_death: Date,
 });
