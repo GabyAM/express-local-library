453aa3f5a3867380a272f6ca3601ad6143bfe8a1
GabyAM <gdgabyam@gmail.com>
2024-02-10 12:04:34 -0300

    implements book instance detail page

--- models/bookInstanceModel.js
+++ models/bookInstanceModel.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const { DateTime } = require("luxon");
+
 const BookInstanceSchema = new Schema({
 	book: { type: Schema.Types.ObjectId, ref: "Book", required: true },
 	imprint: { type: String, required: true },
@@ -15,6 +17,9 @@ const BookInstanceSchema = new Schema({
 
 BookInstanceSchema.virtual("url").get(function () {
 	return `/catalog/bookinstance/${this._id}`;
+});
+BookInstanceSchema.virtual("due_back_formatted").get(function () {
+	return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
 });
 
 const BookInstance = mongoose.model("BookInstance", BookInstanceSchema);
